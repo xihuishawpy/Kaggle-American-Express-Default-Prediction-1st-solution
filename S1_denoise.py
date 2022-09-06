fe1d9f1d3c94ed5cd6f2dcae5778c09bc6839b92
蜡笔小xi <1040310273@qq.com>
2022-09-06 15:09:06 +0000

    update

--- S1_denoise.py
+++ S1_denoise.py
@@ -6,9 +6,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 def denoise(df):
+    # 特征D_63、D_64字典映射
     df['D_63'] = df['D_63'].apply(lambda t: {'CR':0, 'XZ':1, 'XM':2, 'CO':3, 'CL':4, 'XL':5}[t]).astype(np.int8)
     df['D_64'] = df['D_64'].apply(lambda t: {np.nan:-1, 'O':0, '-1':1, 'R':2, 'U':3}[t]).astype(np.int8)
+
     for col in tqdm(df.columns):
         if col not in ['customer_ID','S_2','D_63','D_64']:
             df[col] = np.floor(df[col]*100)
@@ -16,6 +19,7 @@ def denoise(df):
 
 train = pd.read_csv('./input/train_data.csv')
 train = denoise(train)
+# csv转成feather，加速数据读取
 train.to_feather('./input/train.feather')
 
 del train
@@ -23,3 +27,5 @@ del train
 test = pd.read_csv('./input/test_data.csv')
 test = denoise(test)
 test.to_feather('./input/test.feather')
+
+
